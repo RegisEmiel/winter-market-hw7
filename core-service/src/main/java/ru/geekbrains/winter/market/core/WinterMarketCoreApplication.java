d9a4a6716aac680053f3fddfc18eab50e3545523
RegisEmiel <regisemiel@yandex.ru>
2022-03-25 17:55:06 +0300

    Initial commit

---
+++ core-service/src/main/java/ru/geekbrains/winter/market/core/WinterMarketCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.winter.market.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterMarketCoreApplication {
+	// Testing
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterMarketCoreApplication.class, args);
+	}
+}
